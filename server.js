24ad613ba928505f97b5aa1fb3efb48a8e2f70f4
khyiu <superk888@gmail.com>
2021-10-15 21:58:08 +0200

    Simplify Node server config for Heroku

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
 const express = require('express');
 const app = express();
 
-app.use(requireHTTPS);
 app.use(express.static('./dist/heroku-expense-tracker'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/heroku-expense-tracker/'}),
-);
-
 app.listen(process.env.PORT || 8080);
