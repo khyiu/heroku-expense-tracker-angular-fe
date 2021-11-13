389273da04552b72af5177cc676f1c925579fbf1
khyiu <superk888@gmail.com>
2021-11-13 14:01:31 +0100

    Fix linting issues: use single quotes instead of double quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MenuModule} from "./sidebar-menu/menu.module";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MenuModule} from './sidebar-menu/menu.module';
 
 @NgModule({
   declarations: [
--- src/app/sidebar-menu/menu.module.ts
+++ src/app/sidebar-menu/menu.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from "@angular/core";
-import {NavigationMenuComponent} from "./navigation-menu.component";
-import {RouterModule} from "@angular/router";
-import {SharedModule} from "../shared/shared.module";
-import {ImageModule} from "primeng/image";
-import {DividerModule} from "primeng/divider";
+import {NgModule} from '@angular/core';
+import {NavigationMenuComponent} from './navigation-menu.component';
+import {RouterModule} from '@angular/router';
+import {SharedModule} from '../shared/shared.module';
+import {ImageModule} from 'primeng/image';
+import {DividerModule} from 'primeng/divider';
 
 @NgModule({
   declarations: [NavigationMenuComponent],
