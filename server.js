a4856920e2c1251cbb09e424615e800026536ba5
khyiu <superk888@gmail.com>
2021-12-02 01:13:06 +0100

    Revert "Have all requests being redirected to index.html on Heroku so that the Angular app is properly bootstrapped even if the user loads a subsequent URL in the address bar"
    This reverts commit 5731fc0de6a86f9ec23e14b90a5e69b221853839.

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static('./dist/heroku-expense-tracker'));
-app.get('/*', (req, res) => {
-  res.sendFile('./dist/heroku-expense-tracker/index.html');
-})
+app.use(express.static('./dist/heroku-expense-tracker'));
 app.listen(process.env.PORT || 8080);
