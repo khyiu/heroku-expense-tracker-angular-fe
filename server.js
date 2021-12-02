78197f57a9eb0493afe692690265dfa072ede736
khyiu <superk888@gmail.com>
2021-12-02 21:13:12 +0100

    Have all requests being redirected to index.html on Heroku so that the Angular app is properly bootstrapped even if the user loads a subsequent URL in the address bar

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("./dist/heroku-expense-tracker"));
-app.get("*", (req, res) => {
-  res.redirect("/");
+
+// Does redirect all /** request to /, but then, path location strategy isn't working
+// app.get("*", (req, res) => {
+//   res.redirect("/");
+// });
+
+const path = require('path');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/heroku-expense-tracker/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
