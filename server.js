a87f1e32de8b0a1f04346404f4febeb952f2a639
khyiu <superk888@gmail.com>
2021-12-02 19:25:25 +0100

    Have all requests being redirected to index.html on Heroku so that the Angular app is properly bootstrapped even if the user loads a subsequent URL in the address bar

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./dist/heroku-expense-tracker'));
+app.get('*', (req, res) => {
+  res.sendFile('index.html');
+})
 app.listen(process.env.PORT || 8080);
