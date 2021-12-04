64d84d1d6a9773ecada762cd89afcb868ba7f28e
khyiu <superk888@gmail.com>
2021-12-04 13:19:33 +0100

    Fix security hotspot: disclosing ExpressJS fingerprint

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.use(express.static("./dist/heroku-expense-tracker"));
+app.use(express.static('./dist/heroku-expense-tracker'));
+// hide the "x-powered-by" response header that gives up the app is being served by ExpressJS
+app.disable('x-powered-by');
 
 const path = require('path');
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/heroku-expense-tracker/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(
+    path.join(__dirname + '/dist/heroku-expense-tracker/index.html')
+  );
 });
 
 app.listen(process.env.PORT || 8080);
