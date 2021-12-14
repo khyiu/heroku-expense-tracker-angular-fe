c03c98bd5a79679ded431a79f785834f8c1bf08f
khyiu <superk888@gmail.com>
2021-12-14 01:14:33 +0100

    Fix Linting issues

--- src/app/generated-sources/expense-api/api/api.ts
+++ src/app/generated-sources/expense-api/api/api.ts
@@ -1,7 +1,8 @@
 export * from './expense.service';
 import { ExpenseService } from './expense.service';
+import { ExpensesService } from './expenses.service';
+
 export * from './expense.serviceInterface';
 export * from './expenses.service';
-import { ExpensesService } from './expenses.service';
 export * from './expenses.serviceInterface';
 export const APIS = [ExpenseService, ExpensesService];
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
@@ -46,8 +45,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-
+import 'zone.js'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
