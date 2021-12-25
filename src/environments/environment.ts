83b9af3676b43ce125a550dbc50079b2fda25907
khyiu <superk888@gmail.com>
2021-12-25 21:19:24 +0100

    Revert accidental commit of local configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // apiBasePath: 'http://localhost:8080',
-  apiBasePath: 'https://heroku-expense-tracker-back.herokuapp.com',
+  apiBasePath: 'http://localhost:8080',
   logoutRedirectUrl: 'http://localhost:4200',
 };
