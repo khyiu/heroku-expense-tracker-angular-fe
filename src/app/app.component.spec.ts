8c0548a377510aff8963b7621b2c16940cf2089b
khyiu <superk888@gmail.com>
2023-12-30 03:35:45 +0100

    HET-30 Upgrade to Angular 16 - fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { MessageService } from 'primeng/api';
+import {KeycloakService} from 'keycloak-angular';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, TranslateModule.forRoot()],
-      declarations: [AppComponent],
+      imports: [AppComponent, RouterTestingModule, TranslateModule.forRoot()],
+      providers: [MessageService, KeycloakService],
     }).compileComponents();
   });
 
